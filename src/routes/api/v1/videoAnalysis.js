4c31b57061bdae2c23a68df2aa94dce724ac5281
onury5506 <onuryildizsai@gmail.com>
2023-06-14 08:26:09 +0300

    videoAnalysis get route integrated

--- src/routes/api/v1/videoAnalysis.js
+++ src/routes/api/v1/videoAnalysis.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import multer from "multer";
 import { addDataToQueue } from "../../../dataProcess/dataProcess.js";
+import { videoStatuses } from "../../../db/tables/video.consts.js";
+import knex from '../../../db/db.js'
 
 const router = Router()
 
@@ -23,27 +25,47 @@ const fileFilter = function (req, file, cb) {
 const upload = multer({ storage: storage, fileFilter: fileFilter });
 
 router.post("/", upload.single('file'), async (req, res) => {
-    if(!req.file){
+    if (!req.file) {
         return res.sendStatus(400)
     }
 
-    try{
+    try {
         const id = await addDataToQueue(req.file.filename)
 
         res.send({
             id
         })
-    }catch(e){
+    } catch (e) {
         res.sendStatus(500)
     }
 })
 
-router.get("/:id",(req,res)=>{
+router.get("/:id", async (req, res) => {
     const id = req.params.id
-    /*
-        TO-DO
-        get video results
-    */
+    const video = await knex.select("status")
+        .from("videos")
+        .where({
+            id
+        }).first()
+
+    if (!video) {
+        return res.sendStatus(404)
+    }
+    
+    if (video.status != 2) {
+        return res.send({
+            status: videoStatuses[video.status]
+        })
+    }
+
+    const changes = await knex.select("uuid","stepName","startTime","endTime")
+        .from("states")
+        .where({
+            video:id
+        })
+
+    res.send(changes)
+
 })
 
 export default router
